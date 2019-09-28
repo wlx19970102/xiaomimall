ff32aba841717e6e74659140010354a90e894c63
王凌霄 <1241271532@qq.com>
2019-09-28 10:25:28 +0800

    Initial commit

---
+++ src/main/java/com/wlx/xiaomimall/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.wlx.xiaomimall;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(XiaomimallApplication.class);
+    }
+
+}
---
+++ src/main/java/com/wlx/xiaomimall/XiaomimallApplication.java
@@ -0,0 +1,13 @@
+package com.wlx.xiaomimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaomimallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaomimallApplication.class, args);
+    }
+
+}
